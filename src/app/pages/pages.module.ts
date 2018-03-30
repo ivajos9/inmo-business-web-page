225db40fac823edbffd6f9815c12d0c2293e9f90
ivajos9 <ivajosdivz9@gmail.com>
2018-03-30 06:17:22 -0600

    Primer commit

--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -13,13 +13,27 @@ import {AngularFireStorageModule} from 'angularfire2/storage';
 import {BrowserModule} from '@angular/platform-browser';
 import {CommonModule} from '@angular/common';
 import { FhaDetailComponent } from './fha-detail/fha-detail.component';
+import { ImmoFinderComponent } from './immo-finder/immo-finder.component';
+import { ImmoDetailComponent } from './immo-detail/immo-detail.component';
+import { ImmoListComponent } from './immo-list/immo-list.component';
+import { OurServicesComponent } from './our-services/our-services.component';
+import { ContactComponent } from './contact/contact.component';
+import { ProyectosListComponent } from './proyectos-list/proyectos-list.component';
+import { ProyectosDetailComponent } from './proyectos-detail/proyectos-detail.component';
 
 @NgModule ({
   declarations: [
     HomeComponent,
     PagesComponent,
     FhaListComponent,
-    FhaDetailComponent
+    FhaDetailComponent,
+    ImmoFinderComponent,
+    ImmoDetailComponent,
+    ImmoListComponent,
+    OurServicesComponent,
+    ContactComponent,
+    ProyectosListComponent,
+    ProyectosDetailComponent
   ],
   exports: [
     HomeComponent,
