7946666c466b825f68b3a6495a1c574db22a63b1
ivajos9 <ivajosdivz9@gmail.com>
2018-03-31 10:52:20 -0600

    Primer commit

---
+++ src/app/pages/faqs-fha/faqs-fha.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-faqs-fha',
+  templateUrl: './faqs-fha.component.html',
+  styleUrls: []
+})
+export class FaqsFhaComponent implements OnInit {
+
+  constructor() {
+    window.scrollTo(0, 0);
+  }
+
+  ngOnInit() {
+  }
+
+}
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyCRH3gE48-AS_OwYt82BL3XCuSdK6Ejj4I",
+    authDomain: "inmobusinessgt-557bb.firebaseapp.com",
+    databaseURL: "https://inmobusinessgt-557bb.firebaseio.com",
+    projectId: "inmobusinessgt-557bb",
+    storageBucket: "inmobusinessgt-557bb.appspot.com",
+    messagingSenderId: "978170918681"
+  }
 };
